cc60ff23350d48e4bee31f43a3cfeee124e1bb0d
Christopher Anich <2023202@student.cct.ie>
2025-07-21 13:07:27 +0100

    Starting signal-preprocessing unit test

---
+++ tests/test_processor.py
@@ -0,0 +1,8 @@
+# tests/test_preprocessor.py
+
+# Import the required test tools and Numpy.
+import pytest
+import numpy as np
+
+# Import the class to test from the BCI core package.
+from bci_core.preprocessor import SignalPreprocessor 
